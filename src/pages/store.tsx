4e4d4a2327e791856af5a9cd193c0b68887d5771
spreadmycode <spreadmycode@outlook.com>
2021-12-10 16:26:44 -0800

    store updated

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -23,12 +23,12 @@ const Store = () => {
             <section>
                 <div className="w-full flex justify-center items-center">
                     <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-12 px-5 md:px-10 py-8">
-                        <StoreItem image="/images/store1.png" title="3D Gorilla1" price="1000" />
-                        <StoreItem image="/images/store2.png" title="3D Gorilla2" price="500" />
-                        <StoreItem image="/images/store3.png" title="3D Gorilla3" price="100" />
-                        <StoreItem image="/images/store4.png" title="3D Gorilla4" price="800" />
-                        <StoreItem image="/images/store5.png" title="3D Gorilla5" price="2000" />
-                        <StoreItem image="/images/store6.png" title="3D Gorilla6" price="10000" />
+                        <StoreItem image="/images/store1.png" title="3D Gorilla" price="???" />
+                        <StoreItem image="/images/store2.png" title="???" price="???" />
+                        <StoreItem image="/images/store3.png" title="???" price="???" />
+                        <StoreItem image="/images/store4.png" title="???" price="???" />
+                        <StoreItem image="/images/store5.png" title="???" price="???" />
+                        <StoreItem image="/images/store6.png" title="???" price="???" />
                     </div>
                 </div>
             </section>
