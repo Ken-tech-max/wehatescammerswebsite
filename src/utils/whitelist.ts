fcd88e6ab9f7c937a71b0b14ecd6d3c2f6162001
spreadmycode <91585048+spreadmycode@users.noreply.github.com>
2022-02-02 10:37:33 -0800

    pre-sale: whitelist updated

--- src/utils/whitelist.ts
+++ src/utils/whitelist.ts
@@ -44,6 +44,7 @@ export const WHITELIST_OG = [
 ];
 
 export const WHITELIST_WL = [
+    "BwwtBAgCc4tQc1MYuaeCLEBUP679xDB9EcipSDYgpVxK",
     "9Qn87rSRo4kCu9wshT2fuQtzi4qmr9oikUQPTs8hyb1N",
     "3ki4QgHEKkAQNTpFFtWpbdpTZJrc5it1NpRgNsA2gJfa",
     "FDonRJf9D8ZAywHsf2YCsZLvusF46VsRujdEZEG7DveK",
